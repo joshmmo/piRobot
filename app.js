970d71029370a1deb3a16703e149588fb619ed17
joshmmo <josh.moore@infusionsoft.com>
2016-03-08 22:18:37 -0700

    testing gpio module

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+var gpio = require("pi-gpio");
+
+console.log("It's robot time!");
+
+gpio.open(14, "output", function(error) {
+    if (error) {
+        console.log("Error: ", error);
+        return;
+    }
+
+    gpio.write(16, 1, function () {
+        gpio.close(16);
+    });
+});
